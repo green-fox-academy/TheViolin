cf558a75cd63e1817e138ba7df25d1ab186eff0e
Balazs Hegedus <hegedusb93@gmail.com>
2018-10-29 17:07:38 +0100

    Triangle

---
+++ week-01/day-3/triangle.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let star:string="*";
+let lineCount:number = 4;
+
+for (let i:number=1; i <= lineCount; i++){
+    if (i<=lineCount){
+    console.log(star.repeat(i));
+    }
+}
+
+
