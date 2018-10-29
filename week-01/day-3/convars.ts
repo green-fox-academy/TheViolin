3e4ecc6004c6ec454ab2be11ca0f44b8c5e5313c
Balazs Hegedus <hegedusb93@gmail.com>
2018-10-29 14:20:08 +0100

    variable mutatiosn final version

--- week-01/day-3/convars.ts
+++ week-01/day-3/convars.ts
@@ -1,26 +1,26 @@
 let a: number = 24;
 let out: number = 0;
 
-if (a%2 === 0){
+if (a % 2 === 0) {
     out++
 }
 
-console.log (out);
+console.log(out);
 
 let b: number = 13;
 let out2: string = '';
 
-if (b>10 && b<20){
+if (b > 10 && b < 20) {
 
     out2 = "Sweet!";
 }
 
-if (b<10){
+if (b < 10) {
 
     out2 = "Less!";
 }
 
-if (b>20){
+if (b > 20) {
 
     out2 = "More!";
 
@@ -32,21 +32,38 @@ let c: number = 123;
 let credits: number = 100;
 let isBonus: boolean = false;
 
-if (credits>=50 && isBonus==false){
+if (credits >= 50 && isBonus == false) {
 
-    c-=2;
+    c -= 2;
 
 }
 
-else if (credits<50 && isBonus==false){
+else if (credits < 50 && isBonus == false) {
 
     c--;
 
 }
 
-else{
+else {
 
-    c=c;
+    c = c;
 }
 
 console.log(c);
+
+
+let d: number = 8;
+let time: number = 120;
+let out3: string = '';
+
+if (d / 4 == 0 && time >= 200) {
+    out3 = "check";
+}
+
+if (time > 200) {
+    out3 = "Time out";
+} else {
+    out3 = "Run Forest Run";
+}
+
+console.log(out3)
