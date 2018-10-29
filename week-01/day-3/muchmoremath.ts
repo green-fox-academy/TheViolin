739e7abcbb748b5158a4a09899d7ce406071084d
Balazs Hegedus <hegedusb93@gmail.com>
2018-10-29 12:01:22 +0100

    Much more math

---
+++ week-01/day-3/muchmoremath.ts
@@ -0,0 +1,29 @@
+'use strict'
+
+let a = 3;
+let b = 100;
+let c = 44;
+let d = 125;
+let e = 8;
+let f1 = 123;
+let f2 = 345;
+let g1 = 350;
+let g2 = 200;
+let h = 1357988018575474;
+let i1 = 10;
+let i2 = 3;
+let j = 1521;
+let k = "Apple";
+
+console.log (a+10);
+console.log (b-7);
+console.log (c*2);
+console.log (d/5);
+console.log (e**3);
+console.log (g2*2>g1);
+console.log (h%11);
+console.log (i2**3>i1>i2**2);
+console.log (j % 3 ===0); console.log (j % 5 === 0);
+console.log (k.repeat(4));
+
+
