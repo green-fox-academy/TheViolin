017ef6b6e31d223c7096dd17b5a322cc41207668
Balazs Hegedus <hegedusb93@gmail.com>
2018-10-30 11:43:38 +0100

    Factorio

---
+++ week-01/day-4/factorio.ts
@@ -0,0 +1,11 @@
+let num: number=5;
+
+function factorialize(num) {
+    if (num === 0 || num === 1)
+      return 1;
+    for (let i = num - 1; i >= 1; i--) {
+      num *= i;
+    }
+    return num;
+  }
+  console.log (factorialize(num));
