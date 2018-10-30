72f9640e2ece660ebeda352575aa55a227e22c41
Balazs Hegedus <hegedusb93@gmail.com>
2018-10-30 16:12:21 +0100

    Change

--- week-01/day-4/change.ts
+++ week-01/day-4/change.ts
@@ -1,4 +1,13 @@
 let numList: number[]= [1,2,3,8,5,6]
-numList.splice(3, 1, 4);
 
-console.log (numList);
+
+let newList=numList.map((element)=>{
+if (element==8){
+    return element= 4;
+}
+else {
+   return element;
+}
+})
+console.log (newList);
+
