a9c46531fd6f6e4e667ca726e2252933fd29ab94
Balazs Hegedus <hegedusb93@gmail.com>
2018-11-07 17:08:40 +0100

    Checkerboard updated

--- week-02/day-3/checkerboard/checkerboard-ts.ts
+++ week-02/day-3/checkerboard/checkerboard-ts.ts
@@ -6,7 +6,13 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // Fill the canvas with a checkerboard pattern.
-for (var x = 0; x < 15; x++) for (var y = 0; y < 10; y++) {
-  ctx.fillStyle = (x + y) % 2 ? 'white' : 'black';
+for (let x = 0; x < 15; x++) for (let y = 0; y < 10; y++) {
+  
+  if ((x + y) % 2){
+    ctx.fillStyle='white'
+  }
+  else{
+    ctx.fillStyle='black';
+}
   ctx.fillRect(40 * x, 40 * y, 40, 40)
 }
