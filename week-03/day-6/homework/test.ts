718b274a4137d4519d8b118bb1bf72d6da086127
Balazs Hegedus <hegedusb93@gmail.com>
2018-11-18 16:53:30 +0100

    Test.ts updated

--- week-03/day-6/homework/test.ts
+++ week-03/day-6/homework/test.ts
@@ -17,7 +17,10 @@ people.push(gandhi);
 let mentor = new Mentor();
 people.push(mentor);
 let sponsor = new Sponsor();
+people.push(sponsor);
 let elon = new Sponsor('Elon Musk', 46, 'male', 'SpaceX');
+people.push(elon);
+
 
 student.skipDays(3);
 
