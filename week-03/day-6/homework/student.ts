705b0ec14efa29796c64d7f5a195f9cdf6df2c4c
Balazs Hegedus <hegedusb93@gmail.com>
2018-11-18 15:49:18 +0100

    Student.ts added to the homework directory

---
+++ week-03/day-6/homework/student.ts
@@ -0,0 +1,28 @@
+'use strict'
+
+class Student {
+  name: string;
+  age: number;
+  gender: string;
+  previousOrganization: string;
+  skippedDays: number;
+
+  constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female', previousOrganization: string = 'The School of Life', skippedDays: number = 0) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = skippedDays;
+  }
+
+  getGoal() {
+    console.log('Be a junior software developer.')
+  }
+  introduce() {
+    console.log(`Hi, I\'m ${this.name} a ${this.age} old ${this.gender} from ${this.previousOrganization} who skipped ${this.skippedDays} days from the course already.`)
+  }
+  skipDays(numberOfDays) {
+    return this.skipDays + numberOfDays;
+  }
+}
+
