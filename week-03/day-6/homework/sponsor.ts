1c09a6903e2db73148cc9333d9a7c745ed28ed73
Balazs Hegedus <hegedusb93@gmail.com>
2018-11-18 16:08:23 +0100

    Sponsor.ts added to the homework directory

---
+++ week-03/day-6/homework/sponsor.ts
@@ -0,0 +1,30 @@
+'use strict'
+
+class Sponsor {
+
+  name: string;
+  age: number;
+  gender: string;
+  company: string;
+  hiredStudents: number;
+
+  constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female', company: string = 'Google', hiredStudents: number = 0) {
+
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.company = company;
+    this.hiredStudents = hiredStudents;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.name} a ${this.age} old ${this.gender} who represents ${this.company} and hired ${this.hiredStudents} students so far.`)
+  }
+  hire() {
+    return this.hiredStudents++;
+  }
+  getGoal() {
+    console.log('Hire brilliant junior software developers.')
+  }
+}
+
