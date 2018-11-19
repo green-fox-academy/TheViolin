941c5b7ccb97f50acd7fb22f9eaf0186a2740663
Balazs Hegedus <hegedusb93@gmail.com>
2018-11-19 12:17:54 +0100

    feat(Green Fox inheritance exercise) : added cohort.ts

---
+++ week-04/day-1/task1/cohort.ts
@@ -0,0 +1,27 @@
+import { Mentor } from "./mentor";
+import { Student} from "./student";
+
+'use strict';
+
+class Cohort {
+  protected name: string;
+  protected students: Student [];
+  protected mentors: Mentor [];
+
+  constructor (name: string){
+    this.name = name;
+    this.students = [];
+    this.mentors = [];
+  }
+  addStudent (student:Student){
+    this.students.push(student)
+  }
+  addMentor (mentor:Mentor){
+    this.mentors.push(mentor)
+  }
+  info (){
+    console.log (`The ${this.name} cohort has ${this.students.length} students and ${this.mentors.length} mentors.`)
+  }
+}
+
+export { Cohort };
