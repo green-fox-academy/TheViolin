8ec76c853df24ee296893aebf6cb5eb6792d9969
Balazs Hegedus <hegedusb93@gmail.com>
2018-11-21 17:30:44 +0100

    feat(Comparator task) Task second part completed and uploaded to the tas

---
+++ week-04/day-2/thingsort/thing.ts
@@ -0,0 +1,31 @@
+import { Comparable } from "./thingsort";
+
+class Thing implements Comparable {
+  private _name: string;
+  private _completed = false;
+
+  constructor(name: string){
+      this._name = name;
+  }
+
+  public complete() {
+      this._completed = true;
+  }
+
+  get name(){
+      return this._name;
+  }
+  get completed(){
+      return this._completed;
+  }
+  compareTo(other: Thing): number {
+    if (this.completed === other.completed) {
+      return 0;
+    } else if (this.completed > other.completed) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
+}
+export { Thing };
