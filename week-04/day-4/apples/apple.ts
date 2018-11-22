cca1690abf99dd4a92fe60d6c2675e9fdf24efc6
Balazs Hegedus <hegedusb93@gmail.com>
2018-11-22 11:10:41 +0100

    feat(Apples task) apple ts updated, test ts deleted

--- week-04/day-4/apples/apple.ts
+++ week-04/day-4/apples/apple.ts
@@ -1,3 +1,18 @@
-export function getApple() {
-  return 'apple'
+class Apple {
+
+  getApple(): string {
+    return 'apple';
+  }
 }
+let apple = new Apple;
+
+
+import { test } from 'tape';
+
+test('show the apple', t => {
+  const actual = apple.getApple();
+  const expected = 'cica';
+
+  t.equal(actual, expected);
+  t.end();
+});
