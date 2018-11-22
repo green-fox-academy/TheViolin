b748310e43bb483e5e0c668680011081433a9e67
Balazs Hegedus <hegedusb93@gmail.com>
2018-11-22 14:09:01 +0100

    feat(Anagram task) multiple tesk cases created and added to the test.ts

--- week-04/day-4/anagram/test.ts
+++ week-04/day-4/anagram/test.ts
@@ -3,8 +3,24 @@ import { Words } from './anagram';
 
 let words = new Words;
 
-test('show the anagram', t => {
+test('show the anagram with lower case letters', t => {
   const actual = words.anagram('láb','bál');
+  const expected = true;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('show the anagram with lower upper case letters', t => {
+  const actual = words.anagram('BAL','LBA');
+  const expected = true;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('show the anagram with sentences', t => {
+  const actual = words.anagram('áll a bál','láb a lál');
   const expected = true;
 
   t.equal(actual, expected);
