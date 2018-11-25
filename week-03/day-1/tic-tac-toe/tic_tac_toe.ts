2f4852788d740b5e342565411b1ae20c99f1c501
Balazs Hegedus <hegedusb93@gmail.com>
2018-11-25 17:07:07 +0100

    feat(Tic-tac-toe task) forEach deleted, easier version found, solution found (v1.0)

--- week-03/day-1/tic-tac-toe/tic_tac_toe.ts
+++ week-03/day-1/tic-tac-toe/tic_tac_toe.ts
@@ -1,29 +1,28 @@
-export { };                                                
+export { };
 const fs = require('fs');
 
-function readFromFile(fileName: string): string {                
-    try {
-        return fs.readFileSync(fileName, 'utf-8');              
-    } catch (e) {
-        console.log(e.message);
-        return null;
-    }
+function readFromFile(fileName: string): string {
+  try {
+    return fs.readFileSync(fileName, 'utf-8');
+  } catch (fileContent) {
+    console.log(fileContent.message);
+    return null;
+  }
 }
 
-function tictactoe(fileName: string): string{                      
-  let fileContent: string[] = readFromFile(fileName).split('');          
-  let result: string = '';                                       
-  fileContent.forEach(function (e, i) {
-    if (e[0]==='O' && e[1]==='X' && e[2]==='O'){
-      result = 'O';
-    }
-    else if (e[0]==='X' && e[1]==='O' && e[2]==='O'){
-      result = 'X';
-    }
-    else if (e[0]==='X' && e[1]==='O' && e[2]==='X'){
-      result = 'Draw';
-    } 
-  }); return result;
-}
+function tictacResult(fileName: string): string {
+  let fileContent: string[] = readFromFile(fileName).split('');
+  let result: string = '';
 
-console.log(tictactoe("win-o.txt"));
+  if (fileContent[0] === 'O' && fileContent[1] === 'X' && fileContent[2] === 'O') {
+    result = 'O';
+  }
+  else if (fileContent[0] === 'X' && fileContent[1] === 'O' && fileContent[2] === 'O') {
+    result = 'X';
+  }
+  else if (fileContent[0] === 'X' && fileContent[1] === 'O' && fileContent[2] === 'X') {
+    result = 'Draw';
+  }
+  return result;
+}
+console.log(tictacResult("draw.txt"));
