a77c523353273d5d0f93b5a8bf8354421cc848d4
Balazs Hegedus <hegedusb93@gmail.com>
2018-12-06 11:55:49 +0100

    feat(Queryselector1 task) files added, updated, uploaded, task finished

---
+++ week-05/day-4/queryselector-1/mod.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const king = document.querySelector('#b325');
+console.log (king);
+const businessLamp = document.querySelectorAll('.big');
+businessLamp.forEach(e => {
+  console.log(e);
+});
+const conceitedKing = document.querySelectorAll('section div');
+conceitedKing.forEach(e => {
+  alert(e);
+});
+const noBusiness = document.querySelectorAll('body div')
+noBusiness.forEach(e => {
+  console.log(e);
+});
