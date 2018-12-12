898a00a793f77a8733b41c91a84dc1a62389a867
Balazs Hegedus <hegedusb93@gmail.com>
2018-12-12 17:33:59 +0100

    feat(Bubbling task) JS files updated, last modifications

--- week-06/day-3/bubbling/main.js
+++ week-06/day-3/bubbling/main.js
@@ -6,23 +6,25 @@ let photoSize = 200;
 
 buttons.addEventListener('click', event => {
   let direction = event.target.dataset.direction;
-  if (direction === 'in') {
-    photoSize += 10;
-    photo.style.backgroundSize = `${photoSize}%`;
-  } else if (direction === 'out') {
-    photoSize -= 10;
-    photo.style.backgroundSize = `${photoSize}%`;
-  } else if (direction === 'left'&& xPos > - 500) {
-    xPos -= 10;
-    photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
-  } else if (direction === 'right'&& xPos >= -500 && xPos < 0) {
-    xPos += 10;
-    photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
-  } else if (direction === 'up' && yPos >= -320 && yPos <0) {
-    yPos += 10;
-    photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
-  } else if (direction === 'down' && yPos> -320) {
-    yPos -= 10;
-    photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
-}
-})
+  if (photoSize > 100) {
+    if (direction === 'in') {
+      photoSize += 10;
+      photo.style.backgroundSize = `${photoSize}%`;
+    } else if (direction === 'out') {
+      photoSize -= 10;
+      photo.style.backgroundSize = `${photoSize}%`;
+    } else if (direction === 'left' && xPos > - 500) {
+      xPos -= 10;
+      photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
+    } else if (direction === 'right' && xPos >= -500 && xPos < 0) {
+      xPos += 10;
+      photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
+    } else if (direction === 'up' && yPos >= -320 && yPos < 0) {
+      yPos += 10;
+      photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
+    } else if (direction === 'down' && yPos > -320) {
+      yPos -= 10;
+      photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
+    }
+  }
+});
