27a19df8d81c70a01fca2125f82739067b4d3f07
Balazs Hegedus <hegedusb93@gmail.com>
2018-12-12 11:41:27 +0100

    feat(Counter task)files updated, last modification

--- week-06/day-3/counter/main.js
+++ week-06/day-3/counter/main.js
@@ -3,10 +3,10 @@ const items = document.querySelectorAll('li');
 const list = Array.from(items);
 const ps = document.querySelector('p');
 
-console.log (ps)
+console.log(ps)
 
 const listItems = () => {
- console.log (ps.innerHTML = list.length)
-  }
+  console.log(ps.innerHTML = list.length);
+}
 
 button.addEventListener('click', listItems);
