86b455213aaf088abf942c757ac09054661b06ca
Balazs Hegedus <hegedusb93@gmail.com>
2018-12-12 15:57:26 +0100

    feat(Bubbling task) files updated, task completed

---
+++ week-06/day-3/bubbling/main.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelector('nav')
+const photo = document.querySelector('.img-inspector')
+let xPos = 0;
+let yPos = 0;
+let photoSize = 200;
+
+buttons.addEventListener('click', event => {
+  let direction = event.target.dataset.direction;
+  let width = 500;
+  if (direction === 'in') {
+    photoSize += 10;
+    photo.style.backgroundSize = `${photoSize}%`;
+  } else if (direction === 'out') {
+    photoSize -= 10;
+    photo.style.backgroundSize = `${photoSize}%`;
+  } else if (direction === 'left'&& xPos > - 500) {
+    xPos -= 10;
+    photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
+  } else if (direction === 'right'&& xPos >= -500 && xPos < 0) {
+    xPos += 10;
+    photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
+  } else if (direction === 'up' && yPos >= -320 && yPos <0) {
+    yPos += 10;
+    photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
+  } else if (direction === 'down' && yPos> -320) {
+    yPos -= 10;
+    photo.style.backgroundPosition = `${xPos}px ${yPos}px`;
+}
+})
