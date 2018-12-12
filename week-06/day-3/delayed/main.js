fd0826f4a397e3dca89a4753032d1517b405df75
Balazs Hegedus <hegedusb93@gmail.com>
2018-12-12 16:33:37 +0100

    feat(Delayed click task) JS file updated, function reworked

--- week-06/day-3/delayed/main.js
+++ week-06/day-3/delayed/main.js
@@ -1,8 +1,9 @@
 const btn = document.querySelector('button');
 const h1 = document.querySelector('h1');
 
-let text = setTimeout(() => {
-  console.log(h1.innerHTML = '2 seconds ellapsed');
-}, 2000);
-
+let text = () => {
+  setTimeout(() => {
+    console.log(h1.innerHTML = '2 seconds ellapsed');
+  }, 2000);
+}
 btn.addEventListener('click', text);
