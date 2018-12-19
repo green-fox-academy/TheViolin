85071fc817e8f9c34f17ff0f9683003ae95cbdf9
Balazs Hegedus <hegedusb93@gmail.com>
2018-12-19 12:21:02 +0100

    feat(Frontend task) part 2 completed, app.js updated

--- week-07/day-3/frontend/app.js
+++ week-07/day-3/frontend/app.js
@@ -10,6 +10,36 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+  if (req.query.input) {
+    res.send({
+      "received": req.query.input,
+      "result": req.query.input * 2
+    })
+  } else {
+    res.send({
+      "error": "Please provide an input!"
+    })
+  }
+});
+
+app.get('/greeter', (req, res) => {
+  if (req.query.name && req.query.title) {
+    res.send({
+      "welcome_message": `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`
+    })
+  } else if (req.query.name) {
+    res.send({
+      
+      "error": "Please provide a title!"
+    })
+  }else {
+    res.send({
+      "error": "Please provide a name!"
+    })
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT}`);
 });
