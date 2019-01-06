7cee3e84ae7711b9525ef3440d31325f835e2627
Balazs Hegedus <hegedusb93@gmail.com>
2019-01-06 16:38:15 +0100

    feat(Cocktail task) files updated, task finished, css needed

--- week-07/day-1/cocktails/main.js
+++ week-07/day-1/cocktails/main.js
@@ -4,21 +4,34 @@ const PORT = 3000;
 
 app.set('view engine', 'ejs');
 
+const cocktails = [
+  { name: 'GIN FIZZ', price: 1520, contains: ['gin', 'sugar', 'lemon juice', 'soda'], isAlcoholic: true },
+  { name: 'BLOODY MARY', price: 1650, contains: ['vodka', 'tomato juice', 'spices'], isAlcoholic: true },
+  { name: 'SEX ON THE BEACH', price: 1850, contains: ['vodka', 'peach schnapps', 'orange juice', 'cranberry juice'], isAlcoholic: true },
+  { name: 'CUBA LIBRE', price: 1850, contains: ['rum', 'cola', 'lime juice'], isAlcoholic: true },
+  { name: 'MOJITO', price: 1850, contains: ['rum', 'sugar', 'lime juice', 'soda water'], isAlcoholic: true },
+  { name: 'LONG ISLAND ICE TEA', price: 2450, contains: ['vodka', 'rum', 'gin', 'tequila', 'triple sec', 'cola'], isAlcoholic: true },
+  { name: 'VIRGIN MOJITO', price: 990, contains: ['sugar', 'lime juice', 'soda water'], isAlcoholic: false },
+  { name: 'SAFE SEX ON THE BEACH', price: 990, contains: ['peach schnapps', 'orange juice', 'cranberry juice'], isAlcoholic: false },
+];
+
+const alcoholList = ['gin', 'vodka', 'rum', 'tequila'];
+
+
 app.get('/', (req, res) => {
-  const cocktails = [
-    { name: 'GIN FIZZ', price: 1520, contains: ['gin', 'sugar', 'lemon juice', 'soda'], isAlcoholic: true },
-    { name: 'BLOODY MARY', price: 1650, contains: ['vodka', 'tomato juice', 'spices'], isAlcoholic: true },
-    { name: 'SEX ON THE BEACH', price: 1850, contains: ['vodka', 'peach schnapps', 'orange juice', 'cranberry juice'], isAlcoholic: true },
-    { name: 'CUBA LIBRE', price: 1850, contains: ['rum', 'cola', 'lime juice'], isAlcoholic: true },
-    { name: 'MOJITO', price: 1850, contains: ['rum', 'sugar', 'lime juice', 'soda water'], isAlcoholic: true },
-    { name: 'LONG ISLAND ICE TEA', price: 2450, contains: ['vodka', 'rum', 'gin', 'tequila', 'triple sec', 'cola'], isAlcoholic: true },
-    { name: 'VIRGIN MOJITO', price: 990, contains: ['sugar', 'lime juice', 'soda water'], isAlcoholic: false },
-    { name: 'SAFE SEX ON THE BEACH', price: 990, contains: ['peach schnapps', 'orange juice', 'cranberry juice'], isAlcoholic: false },
-  ];
-  const alcoholList = ['gin', 'vodka', 'rum', 'tequila'];
-  res.render('home', {
-    cocktails, alcoholList
-  })
+  let alcohol = req.query.alcohol;
+  if (alcohol) {
+    const resultCocktails = cocktails.filter(cocktail => {
+      return cocktail.contains.includes(alcohol);
+    });
+    res.render('home', {
+      cocktails: resultCocktails, alcohols: alcoholList
+    });
+  } else {
+    res.render('home', {
+      cocktails: cocktails, alcohols: alcoholList
+    });
+  }
 });
 
 app.listen(PORT, () => {
